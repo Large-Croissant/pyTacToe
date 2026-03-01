87892859bce0c8c494f51f9fa9153d162fbce47d
Joey <jpg2368@rit.edu>
2026-03-01 15:52:38 -0500

    added bottom bar and top text, added placeholder for win checking, added reset and exit buttons with functionality

--- main.py
+++ main.py
@@ -1,18 +1,46 @@
-from PySide6.QtWidgets import QApplication, QWidget, QGridLayout, QPushButton
+from PySide6.QtWidgets import QApplication, QWidget, QGridLayout, QPushButton, QVBoxLayout, QHBoxLayout, QLabel
+
+
+def win_check(grid) -> None | str:
+    # check each row
+    # check each col
+    # check diags
+    return None
+
+
+def reset(board, text_label):
+    for row in board:
+        for box in row:
+            box.setText("")
+    text_label.setText("Current Turn: X")
 
 
 def main():
+    # ui setup
     app = QApplication()
     window = QWidget(windowTitle="PyTacToe")
-    window.setFixedSize(350, 350)
-    grid_layout = QGridLayout(parent=window)
-    window.show()
-    boxes = [[QPushButton() for _ in range(3)] for _ in range(3)]
+    v_layout = QVBoxLayout(window)
+    top_text = QLabel("Current Turn: X")
+    v_layout.addWidget(top_text)
+    grid_layout = QGridLayout()
+    v_layout.addLayout(grid_layout)
+    boxes = [[QPushButton("") for _ in range(3)] for _ in range(3)]
     for r, row in enumerate(boxes):
         for c, box in enumerate(row):
             box.setFixedSize(100, 100)
             grid_layout.addWidget(box, r, c)
+    bottom_bar = QHBoxLayout()
+    reset_button = QPushButton("Reset Board")
+    reset_button.clicked.connect(lambda: reset(boxes, top_text))
+    bottom_bar.addWidget(reset_button)
+    exit_button = QPushButton("Exit Game")
+    exit_button.clicked.connect(app.quit)
+    bottom_bar.addWidget(exit_button)
+    v_layout.addLayout(bottom_bar)
+    window.show()
     app.exec()
+
+    current_player = "X"
 
 
 if __name__ == "__main__":
